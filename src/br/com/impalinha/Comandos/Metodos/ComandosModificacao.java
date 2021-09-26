0d54380058a2cabff798952f77d92d81992eb213
Yuri Sanford <75326223+sanfordyuri@users.noreply.github.com>
2021-09-26 19:23:58 -0300

    Finalizado comandos basicos do ADM

---
+++ src/br/com/impalinha/Comandos/Metodos/ComandosModificacao.java
@@ -0,0 +1,13 @@
+package br.com.impalinha.Comandos.Metodos;
+
+import org.bukkit.entity.Player;
+
+import java.math.BigDecimal;
+
+public class Modificacao {
+
+    public static void addCash(Player player, BigDecimal amout) {
+        
+    }
+
+}
