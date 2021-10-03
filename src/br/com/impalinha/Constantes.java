22850eda34423d8f4e50e5da42fa6171357ec152
Yuri Sanford <75326223+sanfordyuri@users.noreply.github.com>
2021-10-03 19:26:03 -0300

    Corrigindo linhas Constantes.java

--- src/br/com/impalinha/Constantes.java
+++ src/br/com/impalinha/Constantes.java
@@ -27,29 +27,22 @@ public class Constantes {
 	public static final String COMANDO_ADD = "add";
 	public static final String[] LETRAS = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "y", "x", "z"};
 	public static final int[] NUMEROS = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
 	public static final String VALOR = ".Valor";
 	public static final String DATA_EXPIRA = ".Data-Expira";
-
 	public static final String INSERT_QUERY = "INSERT INTO `ImpCash`(`Player`, `Amount`) VALUES (?,?)";
 	public static final String ERRO_CRIAR_JOGADOR = "§COps... tivemos um erro ao tentar criar o jogador na tabela.";
-
 	public static final String QUERY_SELECT_AMOUNT = "SELECT `Amount` FROM `ImpCash` WHERE `Player` = ?";
 	public static final String AMOUNT = "Amount";
 	public static final String UPDATE_AMOUNT_QUERY = "UPDATE `ImpCash` SET `Amount` = ? WHERE `Player` = ?";
 	public static final String SELECT_ALL_QUERY = "SELECT * FROM `ImpCash` WHERE `Player` = ?";
-
 	public static final String MYSQL_IP_ANDRESS = "Mysql.ip-andress";
 	public static final String MYSQL_USER = "Mysql.user";
 	public static final String MYSQL_PASSWORD = "Mysql.password";
-
 	public static final String JDBC_MYSQL = "jdbc:mysql://";
 	public static final String SQL_10440256 = ":3306/s203_ImpCash";
-
 	public static final String CREATE_TABLE_QUERY = "CREATE TABLE IF NOT EXISTS `ImpCash`(`Player` VARCHAR(64), `Amount` DOUBLE)";
 	public static final String TABELA_CARREGADA = "§aTabela carregada com sucesso.";
 	public static final String ERRO_AO_CARREGAR_TABELA = "§cErro ao carregar tabela.";
-
 	public static final String PLUGIN_INICIADO = "§aO plugin foi iniciado com sucesso.";
 	public static final String PLUGIN_FINALIZADO = "§cO plugin foi terminado com sucesso.";
 
